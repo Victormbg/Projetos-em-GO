d9a96d0661d9d85566d613001b73df0e5e48c59c
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2020-01-28 20:32:36 +0000

    TUDO ATUALIZADO DIA 28/01

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 PROJETOS USANDO A LINGUAGEM GO
 
 # LINKS 
-https://medium.com/gommunity/configurando-o-visual-studio-code-para-trabalhar-com-go-a465ca941c7
+https://medium.com/gommunity/configurando-o-visual-studio-code-para-trabalhar-com-go-a465ca941c7<br>
+http://goporexemplo.golangbr.org/
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	vs := "Ola mundo"
+	fmt.Println("GO diz", vs)
+
+	var a string = "VICTOR"
+	c := "PROGRAMADOR"
+	var b = 2020
+    
+    fmt.Println(a,c,b)
+    /*IF E ELSE*/
+	if b == 2020 {
+		fmt.Println("O numero e 2020")
+	}else{
+		fmt.Println("O numero nao e 2020")
+	}
+}
